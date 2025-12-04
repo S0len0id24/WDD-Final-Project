c78800da0f3358abdd2ac59359ca4c78f6f67abc
S0len0id69 <khantsithuhein223@gmail.com>
2025-12-04 14:01:54 +0000

    Adding JS for html validation

--- mypage.js
+++ mypage.js
@@ -15,3 +15,31 @@ function showMore() {
 }
 
 
+
+function validateForm() {
+
+    // Name variable to radio buttons (with checked or not)
+    var cause1 = document.getElementById("cause1").checked;
+    var cause2 = document.getElementById("cause2").checked;
+    var cause3 = document.getElementById("cause3").checked;
+    var cause4 = document.getElementById("cause4").checked;
+
+    // Get textarea value by giving variable
+    var feedback = document.getElementById("feedback").value;
+
+    // If there is no radio button selected , the alert comes out
+    if (!cause1 & !cause2 & !cause3 & !cause4) {
+        alert("Please choose one cause of climate change.");
+        return false; // STOP submitting
+    }
+
+    // If textarea empty
+    if (feedback.trim() == "") {
+        alert("Please write your ideas or suggestions.");
+        return false; // STOP submitting
+    }
+
+    //This part says true after choosing radio buttons and filling ideas in textarea
+    alert("Thank you for your feedback!");
+    return true; //it approves submitting
+}
